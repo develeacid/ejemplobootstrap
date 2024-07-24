142af5a587ce7b1d1df29c1991150f6682414dc8
eleacid <eleacid@prueba.com>
2024-07-24 11:18:14 -0600

    version estable sin importar nada, solo bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,3 @@
 import "./bootstrap";
-import "../css/app.scss";
 import * as bootstrap from "bootstrap";
-
-import $ from "jquery";
-window.$ = $;
-window.jQuery = $;
-
-import "./lib/waypoints.min";
-import "./lib/easing";
-import "./lib/owl.carousel";
-import "./lib/counterup.min";
-//import "./lib/wow";
-
-import "./main"; // Tu script principal
+import "../css/app.scss";
--- vite.config.js
+++ vite.config.js
@@ -4,28 +4,8 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.scss",
-                "resources/js/app.js",
-                "resources/js/lib/counterup.min",
-                "resources/js/lib/easing",
-                "resources/js/lib/owl.carousel",
-                "resources/js/lib/waypoints.min",
-                "resources/js/lib/wow",
-            ],
+            input: ["resources/js/app.js", "resources/css/app.scss"],
             refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            $: "jquery",
-        },
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: '@import "bootstrap/scss/bootstrap";', // importa bootstrap
-            },
-        },
-    },
 });
